023fafe6e0b2dd507bce64b1db46f2413943754c
dansalazar97 <145073244+dansalazar97@users.noreply.github.com>
2023-09-15 00:01:55 +0000

    Enhance: Project

---
+++ server/app.js
@@ -0,0 +1,50 @@
+var createError = require('http-errors');
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+var debug = require('debug')('dwpc2:server');
+
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
+
+// creando la instancia de express
+var app = express();
+
+// configurando el motor de plantillas
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'hbs');
+
+// se establecen los middlewares
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+// crear un server de archivos estáticos
+app.use(express.static(path.join(__dirname, '..', 'public')));
+
+app.use('/', indexRouter);
+// activa "usersRouter" cuando se
+// solicita "/users"
+app.use('/users', usersRouter);
+/* app.use('/author', (req, res) => {
+  res.json({mainDeveloper: "Daniel Salazar"})
+}) */
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+module.exports = app;
